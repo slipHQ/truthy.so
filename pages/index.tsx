a336df2fcf37860cd43c65317557ef1c88e576b2
Callum McIntyre <callum.mcintyre@procensus.com>
2021-10-15 22:18:33 +0100

    Install + Configure Tailwind
    Following https://tailwindcss.com/docs/guides/nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 className="bg-red-200">Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
