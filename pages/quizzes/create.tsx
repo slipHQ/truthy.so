f32d709d4daabe8433bda62394b65c87046295b0
Callum McIntyre <callum.mcintyre@procensus.com>
2021-10-17 08:19:36 +0100

    Add supabase auth with Github + scaffold for /quizzes/create page (which is authed)

---
+++ pages/quizzes/create.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import SignInButton from "../../components/SignInButton";
+import { supabase } from "../../utils/supabaseClient";
+
+export default function Create() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  async function signout() {
+    const { error } = await supabase.auth.signOut();
+    if(error) throw error
+  }
+
+  if (session) {
+    console.log(session.user);
+    return (
+      <>
+        <h1>Create UI!</h1>
+        <button onMouseDown={signout}>Log out</button>
+
+        <div className="max-w-4xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
+          <div className="max-w-3xl mx-auto">
+            <p>Create quiz UI here!</p>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Log in to create a quiz!</h1>
+        <SignInButton />
+      </>
+    );
+  }
+}
