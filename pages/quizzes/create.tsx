b0bc92d5f60e150ed32940b7b738b5c4a6f1183c
Callum McIntyre <mcintyre1994@gmail.com>
2021-11-02 22:49:13 +0000

    Style sign in and log out UIs

--- pages/quizzes/create.tsx
+++ pages/quizzes/create.tsx
@@ -4,11 +4,10 @@ import CreateQuizForm from "../../components/CreateQuizForm";
 import SignInButton from "../../components/SignInButton";
 import PageTitle from "../../components/PageTitle";
 import { supabase } from "../../utils/supabaseClient";
-import { useRouter } from 'next/router'
-import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function Create() {
-  const [session, setSession] = useState<Session>(null);
+export default function Create({ serverSideSession }) {
+  const [session, setSession] = useState<Session>(serverSideSession);
 
   useEffect(() => {
     setSession(supabase.auth.session());
@@ -20,20 +19,31 @@ export default function Create() {
 
   async function signout() {
     const { error } = await supabase.auth.signOut();
-    if(error) throw error
+    if (error) throw error;
   }
 
-  const router = useRouter()
-  const query = router.query
-  if(query.error && query.error == "server_error" && query.error_description && query.error_description == "Error getting user email from external provider") {
-    alert("Sorry but we were unable to log you in. Please ensure that you have set an email address in Twitter and try again!")
+  const router = useRouter();
+  const query = router.query;
+  if (
+    query.error &&
+    query.error == "server_error" &&
+    query.error_description &&
+    query.error_description == "Error getting user email from external provider"
+  ) {
+    alert(
+      "Sorry but we were unable to log you in. Please ensure that you have set an email address in Twitter and try again!"
+    );
   }
 
   if (session) {
-    console.log(session.user);
     return (
       <>
-        {/* <button onMouseDown={signout}>Log out</button> */}
+        <button
+          className="absolute top-8 md:top-[35px] right-8 md:right-[120px] bg-gray-900 text-white px-2 py-2 rounded-md text-sm"
+          onMouseDown={signout}
+        >
+          Log out
+        </button>
         <div className="max-w-4xl px-4 py-40 mx-auto sm:px-6 lg:px-8">
           <div className="max-w-3xl mx-auto">
             <PageTitle>Create Quiz</PageTitle>
@@ -45,8 +55,12 @@ export default function Create() {
   } else {
     return (
       <>
-        <h1>Log in to create a quiz!</h1>
-        <SignInButton />
+        <div className="max-w-4xl px-4 py-40 mx-auto sm:px-6 lg:px-8">
+          <div className="max-w-3xl mx-auto">
+            <PageTitle>Log in to create a quiz</PageTitle>
+            <SignInButton />
+          </div>
+        </div>
       </>
     );
   }
