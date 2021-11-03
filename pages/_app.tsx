f66108579a4c8f2b75c8b410272b1ed4cd7c4d81
Callum McIntyre <mcintyre1994@gmail.com>
2021-11-03 15:39:41 +0000

    Add favicon images from realfavicongenerator.net

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import Image from "next/image";
+import Head from "next/head";
 
 import "tailwindcss/tailwind.css";
 import { useFathom } from "../hooks/use-fathom";
@@ -9,9 +9,32 @@ function MyApp({ Component, pageProps }: AppProps) {
   useFathom("UKFLVOQJ", { includedDomains: ["truthy.so", "www.truthy.so"] });
   return (
     <>
-      <div className='m-4 top-8 md:top-[35px] left-8 md:left-[120px]'>
-        <a href='/'>
-          <img src='/logo.png' className='w-8 sm:w-12' />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#333333" />
+      </Head>
+      <div className="m-4 top-8 md:top-[35px] left-8 md:left-[120px]">
+        <a href="/">
+          <img src="/logo.png" className="w-8 sm:w-12" />
         </a>
       </div>
       <Component {...pageProps} />
